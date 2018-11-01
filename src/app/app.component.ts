2937007476d988b4822ad13aaa2326ad7b93dceb
Will Reichl <William.Reichl@dcsg.com>
2018-10-31 21:52:22 -0400

    Added ngFor components for each odd/even number, just need to pass numbers now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
   onNumberChanged(numberData: { gameNum: number} ) {
     console.log('Game running: ' + numberData.gameNum);
+    if (numberData.gameNum % 2 === 1) {
+      this.oddNumbers.push(numberData.gameNum);
+    } else {
+      this.evenNumbers.push(numberData.gameNum);
+    }
   }
 }
 
